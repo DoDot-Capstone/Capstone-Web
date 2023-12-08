69b96f7610b032f593bbb9b4ff90c9a837e844d2
SeonJae <william199@naver.com>
2023-12-09 00:54:56 +0900

    추천한 라이브러리가 없는 경우 결과 추가

--- python/gpt/chatgpt.py
+++ python/gpt/chatgpt.py
@@ -18,7 +18,7 @@ def response(message):
         messages=[
             {"role": "system", "content": "프로그래밍할 때 필요한 라이브러리를 추천해줍니다."},
             {"role": "user", "content": message},
-            {"role": "user", "content": "프로그래밍할 때 필요한 라이브러리만 추천해줘야해, 무조건 라이브러리 이름에 해당하는 부분은 양 옆에 !!!를 넣어서 표시해줘. 코드 자료는 필요 없어"}
+            {"role": "assistant", "content": "프로그래밍할 때 필요한 라이브러리만 추천해줘야해, 무조건 라이브러리 이름에 해당하는 부분은 양 옆에 !!!를 넣어서 표시해줘. 코드 자료는 필요 없어"}
         ],
         temperature=0,
         max_tokens=256
